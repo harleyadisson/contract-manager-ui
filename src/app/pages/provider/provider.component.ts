164a045d1d75a56686bf6f9c5b211be058162a16
harleyadisson <harleyadisson@outlook.com>
2021-08-21 17:26:55 -0300

    Cadastro de prestador de serviço funcionando

--- src/app/pages/provider/provider.component.ts
+++ src/app/pages/provider/provider.component.ts
@@ -1,3 +1,4 @@
+import { query } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NzButtonSize } from 'ng-zorro-antd/button';
@@ -28,7 +29,42 @@ export class ProviderComponent implements OnInit {
         this.validateForm.controls[i].updateValueAndValidity();
       }
     }
-    console.log(this.validateForm.value)
+
+    console.log(this.validateForm.controls)
+
+    fetch('http://localhost:3000/provider', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          providerType: this.validateForm.controls.documentType.value,
+          document: this.validateForm.controls.document.value,
+          name: this.validateForm.controls.name.value,
+          email: this.validateForm.controls.email.value,
+          address: {
+            cep: this.validateForm.controls.cep.value,
+            logradouro: this.validateForm.controls.logradouro.value,
+            complemento: this.validateForm.controls.complemento.value,
+            bairro: this.validateForm.controls.bairro.value,
+            localidade: this.validateForm.controls.localidade.value,
+            uf: this.validateForm.controls.uf.value,
+            ibge: "",
+            gia: "",
+            ddd: "",
+            siafi: ""
+          }
+        }),
+    })
+      .then(data => {
+        return data.json()
+      })
+      .then(response => {
+        console.log(response)
+      });
+
   }
 
   ngOnInit() {
@@ -39,9 +75,9 @@ export class ProviderComponent implements OnInit {
       cep: [null, [Validators.required, Validators.pattern("^[0-9]*$")]],
       logradouro: [null, [Validators.required]],
       numero: [null, [Validators.required]],
-      complemento: [null, [Validators.required]],
+      complemento: [null],
       bairro: [null, [Validators.required]],
-      cidade: [null, [Validators.required]],
+      localidade: [null, [Validators.required]],
       uf: [null, [Validators.required]],
       documentType: [null, [Validators.required]],
     });
@@ -50,27 +86,27 @@ export class ProviderComponent implements OnInit {
 
   onKey(event: any) {
     if (event.code === "Backspace") {
-      if(this.cepCounter > 0)
+      if (this.cepCounter > 0)
         this.cepCounter -= 1;
     } else {
-        this.cepCounter += 1;
+      this.cepCounter += 1;
     }
-     
+
     if (this.cepCounter == 8) {
       console.log(this.validateForm.controls.cep.value)
       fetch(`http://localhost:3000/address?cep=${this.validateForm.controls.cep.value}`)
-      .then(data => {
-        return data.json()
-      })
-      .then(address => {
-        console.log(address)
-        this.validateForm.patchValue({
-          logradouro: address.logradouro,
-          cidade: address.localidade,
-          bairro: address.bairro,
-          uf: address.uf
-        });
-      })
+        .then(data => {
+          return data.json()
+        })
+        .then(address => {
+          console.log(address)
+          this.validateForm.patchValue({
+            logradouro: address.logradouro,
+            localidade: address.localidade,
+            bairro: address.bairro,
+            uf: address.uf
+          });
+        })
     }
 
     console.log(this.cepCounter)
